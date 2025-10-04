8a642d9dbb6f44d0b8a017ea31cf3852dcd316e6
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-10-04 20:18:04 +0100

    ConfigureSlefServiceProject

--- VisitorManagementSystem.Self-Service-UI/Program.cs
+++ VisitorManagementSystem.Self-Service-UI/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using VisitorManagementSystem.Client;
+using VisitorManagementSystem.Self_Service_UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBlazorBootstrap();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
