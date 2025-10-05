59dda363ab183073936e5604303ff0b256d5c269
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-10-05 14:41:32 +0100

    AddNewVisitorEditFormAndMakeCreateVisitDetailToClass

--- Shared/DataTransferObjects/CreateVisitDetailDto.cs
+++ Shared/DataTransferObjects/CreateVisitDetailDto.cs
@@ -1,29 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Shared.DataTransferObjects;
 
 public record class CreateVisitDetailDto
-(
+{
     [Required(ErrorMessage = "Visitor Name is a required field.")]
-    string VisitorName,
+    public string VisitorName { get; set; }
     [Required(ErrorMessage = "Visitor Phone Number is a required field.")]
-    string VisitorPhoneNumber,
-    string VisitorEmailAddress,
+    public string VisitorPhoneNumber { get; set; }
+    public string VisitorEmailAddress { get; set; }
     [Required(ErrorMessage = "Purpose Of Visit is a required field.")]
-    string PurposeOfVisit,
+    public string PurposeOfVisit { get; set; }
     [Required(ErrorMessage = "Host Name is a required field.")]
-    string HostName,
+    public string HostName { get; set; }
     [Required(ErrorMessage = "Visitation Date is a required field.")]
-    DateOnly VisitationDate,
+    public DateOnly VisitationDate { get; set; }
     [Required(ErrorMessage = "Visit Type is a required field.")]
-    string VisitType,
+    public string VisitType { get; set; }
     [Required(ErrorMessage = "Visit Registration Type is a required field.")]
-    string VisitorRegistrationType,
+    public string VisitorRegistrationType { get; set; }
     [Required(ErrorMessage = "Visitor Gender is a required field.")]
-    string VisitorGender
-);
+    public string VisitorGender { get; set; }
+}
