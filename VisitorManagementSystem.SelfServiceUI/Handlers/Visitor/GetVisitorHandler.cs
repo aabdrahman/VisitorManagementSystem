1ed18f0ec1e19271ba27964d265466859970c1e8
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-10-06 10:10:35 +0100

    AddGetByPhoneNumberHandler

---
+++ VisitorManagementSystem.SelfServiceUI/Handlers/Visitor/GetVisitorHandler.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Response;
+using Shared.DataTransferObjects;
+using System.Text.Json;
+
+namespace VisitorManagementSystem.SelfServiceUI.Handlers.Visitor;
+
+public class GetVisitorHandler
+{
+    private HttpClient _httpClient;
+
+    public GetVisitorHandler(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("ApiClient");
+    }
+
+    public async Task<(string respMsg, VisitorDto? respDetails)> Handle(string phoneNumber)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"api/visitor?phoneNumber={phoneNumber}");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return (respMsg:$"Error Fetching Details. Status Code: {(int)response.StatusCode}", null);
+            }
+
+            var responseDetails = await response.Content.ReadAsStringAsync();
+
+            var responseDet = JsonSerializer.Deserialize<Response>(responseDetails);
+
+            var resp = (VisitorDto)responseDet.ResponseData;
+
+            return (respMsg: "Success", respDetails: resp);
+
+        }
+        catch (Exception ex)
+        {
+            return (respMsg: "An Error Occurred Fetching", null);
+        }
+
+    }
+}
--- VisitorManagementSystem.SelfServiceUI/Program.cs
+++ VisitorManagementSystem.SelfServiceUI/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using VisitorManagementSystem.SelfServiceUI;
+using VisitorManagementSystem.SelfServiceUI.Handlers.Visitor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//Services and Handlers
+builder.Services.AddScoped<GetVisitorHandler>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient("ApiClient", opts =>
+{
+    opts.BaseAddress = new Uri("http://localhost:5081");
+});
 
 await builder.Build().RunAsync();
