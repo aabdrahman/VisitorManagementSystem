25fa7687b76a891abb8ac5bc48c79a4dc65118e8
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-10-13 11:37:23 +0100

    AddControllerProject

---
+++ VisitorManagementSystem.Presentation/AuthenticationController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorManagementSystem.Presentation.ActionFilters;
+
+namespace VisitorManagementSystem.Presentation;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public AuthenticationController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPassword([FromBody] ChangePasswordDto changePasswordDetails)
+    {
+        var result = await _serviceManager.AuthenticationService.ResetPassword(changePasswordDetails);
+
+        return Ok(result);
+    }
+
+    [HttpPost("createRole")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [Authorize(Roles = "ReceptionistPolicy")]
+    public async Task<IActionResult> CreateRole([FromBody] RoleForRegistrationDto roleForRegistration)
+    {
+        //if(!ModelState.IsValid)
+        //{
+        //    return BadRequest(ModelState);
+        //}
+        //string accessToken;
+        //var token = HttpContext.Request.Headers.TryGetValue("Authorization",)
+
+        var headerAuth = HttpContext.Request.Headers["Authorization"].ToString();
+        if(string.IsNullOrEmpty(headerAuth))
+        {
+            return Unauthorized("No Token Passed.");
+        }
+
+        var tokenHeaders = headerAuth.Split(' ')[1];
+        if(string.IsNullOrEmpty(tokenHeaders))
+        {
+            return Unauthorized("Empty Token");
+        }
+        var token = tokenHeaders;
+
+        var result = await _serviceManager.AuthenticationService.CreateRole(roleForRegistration, token);
+
+        if(!result.Succeeded)
+        {
+            foreach(var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        return Created();
+    }
+
+    [HttpGet("getRoles")]
+    [Authorize(Policy = "AdminPolicy")]
+    public async Task<IActionResult> GetRoles()
+    {
+        var result = await _serviceManager.AuthenticationService.GetRoles();
+
+        return Ok(result);
+    }
+
+    [HttpPost("RegisterUser")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [Authorize(Policy = "AdminPolicy")]
+    public async Task<IActionResult> RegisterUser([FromBody] UserForCreationDto userForCreation)
+    {
+        var result = await _serviceManager.AuthenticationService.CreateUser(userForCreation);
+
+        if(!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> ValidateUser([FromBody] UserToLoginDto userToLogin)
+    {
+        var result = await _serviceManager.AuthenticationService.ValidateUser(userToLogin);
+
+        if (!result)
+            return Unauthorized("Invalid Username or Password.");
+
+        var token = await _serviceManager.AuthenticationService.GenerateToken(populateExp: true);
+
+        return Ok(token);
+    }
+
+    [HttpPost("refresh")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> RefreshToken(TokenDto tokenDto)
+    {
+        var result = await _serviceManager.AuthenticationService.GenerateRefreshToken(tokenDto);
+
+        return Ok(result);
+    }
+}
+
+
---
+++ VisitorManagementSystem.Presentation/VisitDetailController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using Shared.RequestFeatures;
+using System.Text.Json;
+using VisitorManagementSystem.Presentation.ActionFilters;
+
+namespace VisitorManagementSystem.Presentation;
+
+[Route("api/visitdetail")]
+public class VisitDetailController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public VisitDetailController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    [HttpGet]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> GetAllVisits([FromQuery] VisitDetailRequestParameter visitDetailRequestParameter)
+    {
+        var response = await _serviceManager.VisitDetailService.GetAllVisits(visitDetailRequestParameter, trackChanges: false, ignoreQueryFilter: true);
+        // Request.Headers.Append("X-Pagination", JsonSerializer.Serialize(response.metaData));
+        Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(response.metaData));
+        return Ok(response.visits);
+    }
+
+    [HttpGet("{VisitorIdentificationNumber}", Name = "GetById")]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> GetVisitDetailByVisitorIdentificationNumber(string VisitorIdentificationNumber)
+    {
+        var result = await _serviceManager.VisitDetailService.GetVisitDetailsByIdentificationNumber(VisitorIdentificationNumber, trackChanges: false, ignoreQueryFilter: true);
+
+        return Ok(result);
+    }
+
+    [HttpPost("scheduleVisit")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> ScheduleVisit([FromBody] ScheduleVisitDetailDto scheduleVisitDetail)
+    {
+        var result = await _serviceManager.VisitDetailService.ScheduleVisit(scheduleVisitDetail);
+
+        return CreatedAtRoute("GetById", new { VisitorIdentificationNumber = result.VisitorIdentificationNumber }, result);
+        //return Ok(result);
+    }
+
+    [HttpPost("update-status")]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    public async Task<IActionResult> UpdateVisitStatus([FromBody] UpdateVisitStatusDto updateVisitStatus)
+    {
+        var response = await _serviceManager.VisitDetailService.UpdateStatus(updateVisitStatus);
+
+        return Ok(response);
+    }
+
+    [HttpPost("create-walkin")]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    public async Task<IActionResult> CreateWalkIn([FromBody] CreateVisitDetailDto createVisitDetail)
+    {
+        var result = await _serviceManager.VisitDetailService.CreateVisit(createVisitDetail);
+
+        return Ok(result);
+    }
+
+    [HttpPut("checkIn")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> CheckInVisitor([FromBody] VisitorDetailsCheckInDto visitorDetailsCheckIn)
+    {
+        var result = await _serviceManager.VisitDetailService.UpdateCheckIn(visitorDetailsCheckIn);
+
+        return Ok(result);
+    }
+
+    [HttpPut("checkOut")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    [Authorize(Policy = "ReceptionistPolicy")]
+    [EnableRateLimiting(policyName: "SpecialPolicy")]
+    public async Task<IActionResult> CheckOutVisitor([FromBody] VisitorDetailsCheckInDto checkOutDetails)
+    {
+        var result = await _serviceManager.VisitDetailService.UpdateCheckOut(checkOutDetails);
+
+        return Ok(result);
+    }
+
+}
