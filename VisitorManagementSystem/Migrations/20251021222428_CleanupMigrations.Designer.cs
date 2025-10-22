519b8ce3e0737e0c7c3cdc3bd4c195003bf7f987
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-10-22 11:07:03 +0100

    AddMigrationToCreateProcedureForUserSmummary

--- VisitorManagementSystem/Migrations/20251021222428_CleanupMigrations.Designer.cs
+++ VisitorManagementSystem/Migrations/20251021222428_CleanupMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -11,9 +12,11 @@ using Repository;
 namespace VisitorManagementSystem.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20251021222428_CleanupMigrations")]
+    partial class CleanupMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VisitorManagementSystem/Migrations/20251022091554_AddGetUserSummaryDetailsProc.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VisitorManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetUserSummaryDetailsProc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER PROCEDURE [dbo].[GetUserSummaryDetails]
+	                    @NumberOfRecords INT,
+	                    @NumberOfRecordsToSkip INT,
+	                    @RoleName NVARCHAR(50),
+	                    @Username NVARCHAR(100) = ''
+                  AS
+
+                BEGIN
+	                SET NOCOUNT ON;
+		                SELECT [u].[StaffId], [u].[FirstName], [u].[LastName], [u].[Email], [u].[PhoneNumber], [u].[isActive], [r].[Name], [u].[CreatedAt]
+		                FROM dbo.AspNetUsers u
+		                JOIN dbo.AspNetUserRoles ur 
+		                ON u.Id = ur.UserId
+		                INNER JOIN dbo.AspNetRoles r
+		                ON r.Id = ur.RoleId
+                        WHERE 
+                        (
+                            (@RoleName = '' OR r.Name LIKE '%' + @RoleName + '%')
+                            AND (@Username = '' OR u.UserName LIKE '%' + @Username + '%')
+                        )
+		                ORDER BY u.CreatedAt
+		                OFFSET @NumberOfRecordsToSkip ROWS
+		                FETCH NEXT @NumberOfRecords ROWS ONLY
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE IF EXISTS [dbo].[GetUserSummaryDetails]");
+        }
+    }
+}
